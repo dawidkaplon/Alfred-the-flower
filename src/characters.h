d46d6551e371b22cb92f6da1ccd7b014b8ddf76b
dawidkaplon <dawidkaplon2@gmail.com>
2024-04-14 21:42:13 +0200

    Add .h file with custom characters

---
+++ src/characters.h
@@ -0,0 +1,43 @@
+#ifndef CHARACTERS
+#define CHARACTERS
+
+// Custom characters
+
+// id=0
+unsigned char sadFace[8] = {
+	0b00000,
+    0b01010,
+    0b01010,
+    0b00000,
+    0b01110,
+    0b10001,
+    0b10001,
+    0b00000
+};
+
+
+// id=1
+unsigned char happyFace[8] = {
+	0b00000,
+	0b11011,
+	0b11011,
+	0b00000,
+	0b10001,
+	0b10001,
+	0b01110,
+	0b00000
+};
+
+// id=2
+unsigned char heart[8] = {
+	0b00000,
+	0b00000,
+	0b01010,
+	0b11111,
+	0b11111,
+	0b01110,
+	0b00100,
+	0b00000
+};
+
+#endif
